8181f6e687818e40a1fe465d80c239548098b7e7
Octaniko <sergeitananin@gmail.com>
2025-11-18 20:07:13 +0300

    feat: Set up obstacle layer, smooth out the unit movement

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var target: Node2D = null
 
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	call_deferred("seeker_setup")
@@ -14,9 +15,20 @@ func seeker_setup():
 		navigation_agent_2d.target_position = target.global_position
 
 func _physics_process(delta: float) -> void:
+	if target:
+		navigation_agent_2d.target_position = target.global_position
 	if navigation_agent_2d.is_navigation_finished():
 		return
 	var current_agent_position = global_position
 	var next_path_position = navigation_agent_2d.get_next_path_position()
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	var new_velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
 	move_and_slide()
+	sprite_2d.flip_h = false if velocity.x >0 else true
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
