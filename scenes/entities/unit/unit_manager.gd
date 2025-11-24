d50fe366cc79ac3bbe3f0b27ff3a3534bbfe96c3
Octaniko <sergeitananin@gmail.com>
2025-11-24 16:03:36 +0300

    refactor: Made the unit counting architecture better and fixed it

--- scenes/entities/unit/unit_manager.gd
+++ scenes/entities/unit/unit_manager.gd
@@ -6,19 +6,34 @@ extends Node
 
 var units_reserved := 0
 
+signal units_changed(units_active: int, units_reserced: int, units_total: int)
+
 func _ready() -> void:
 	Global.interactable_collected.connect(Callable(self, "_on_interactable_collected"))
 
 func _on_interactable_collected():
 	units_reserved += 1
+	_emit_units_changed()
 
 func _process(delta: float) -> void:
 	var units_active = get_tree().get_nodes_in_group("units").size()
 	if units_active < unit_limit and units_reserved > 0:
 		spawn_unit()
 		units_reserved -= 1
+		_emit_units_changed()
 
 func spawn_unit():
 	var unit_instance = unit_scene.instantiate()
-	unit_instance.position = base.global_position
+	unit_instance.global_position = base.global_position
 	add_child(unit_instance)
+
+func _emit_units_changed():
+	var units_active = get_tree().get_nodes_in_group("units").size()
+	var units_total = units_active + units_reserved
+	emit_signal("units_changed", units_active, units_reserved, units_total)
+
+func get_reserved() -> int:
+	return units_reserved
+
+func get_active() -> int:
+	return get_tree().get_nodes_in_group("units").size()
--- ui/unit_counter.gd
+++ ui/unit_counter.gd
@@ -1,17 +1,16 @@
 extends HBoxContainer
 
-@onready var unit_counter: Label = $UnitCounter
-
-var units_active: int
-var units_reserved: int
-var units_total: int
+@onready var unit_counter: Label = $VBoxContainer/UnitCounter
+@onready var unit_manager: Node = $"../../../UnitManager"
 
 func _process(delta: float) -> void:
-	_update_unit_counter()
+	if unit_manager:
+		unit_manager.connect("units_changed", Callable(self, "_on_units_changed"))
+	_update_unit_counter(unit_manager.get_active(), unit_manager.get_reserved())
 
-func _update_unit_counter():
-	units_active = get_tree().get_nodes_in_group("units").size()
-	# todo make these work
-	units_reserved = get_tree().get_nodes_in_group("units").size()
-	units_total = units_active + units_reserved
+func _on_units_changed(units_active: int, units_reserved: int, units_total: int) -> void:
+	_update_unit_counter(units_active, units_reserved)
+
+func _update_unit_counter(units_active: int, units_reserved: int):
+	var units_total = units_active + units_reserved
 	unit_counter.text = "%d/%d/%d" % [units_active, units_reserved,units_total]
