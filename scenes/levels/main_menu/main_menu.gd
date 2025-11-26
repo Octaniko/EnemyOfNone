4d1d55bb1fd2c17f9ac7a237697d4e8250bc6ac3
Octaniko <sergeitananin@gmail.com>
2025-11-26 12:14:17 +0300

    feat: Add volume slider

--- scenes/levels/main_menu/main_menu.gd
+++ scenes/levels/main_menu/main_menu.gd
@@ -2,8 +2,15 @@ extends Control
 
 @onready var intro: VideoStreamPlayer = $CanvasLayer/Control/Intro
 @onready var loop: VideoStreamPlayer = $CanvasLayer/Control/Loop
+@onready var volume_slider: HSlider = $CanvasLayer/VBoxContainer2/VolumeSlider
 
 const WORLD = preload("res://scenes/levels/world.tscn")
+
+func _ready() -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"),
+		volume_slider.value
+	)
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(WORLD)
@@ -13,3 +20,9 @@ func _on_quit_button_pressed() -> void:
 
 func _on_intro_finished() -> void:
 	intro.visible = false
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		AudioServer.get_bus_index("Master"),
+		value
+	)
