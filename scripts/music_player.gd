c9277d222c3c9a5108601865d502bbbc789c3594
Octaniko <sergeitananin@gmail.com>
2025-11-26 11:51:17 +0300

    feat: Add music shuffling

--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -6,19 +6,30 @@ const MUSIC_TRACKS = [
 	preload("res://assets/music/BC 022 Valley of Repose (Olimar) .mp3"),
 	preload("res://assets/music/BC 161 Perplexing Pool (Olimar) .mp3"),
 	preload("res://assets/music/36 - BIT ROOTS.mp3"),
-	preload("res://assets/music/35 - GLACEIR.mp3")
-	
+	preload("res://assets/music/35 - GLACEIR.mp3"),
+	preload("res://assets/music/BC 080 Awakening Wood (Olimar) .mp3"),
 ]
+
+var playlist = []
+var current_index = 0
 
 func _ready():
 	randomize()
+	_shuffle_playlist()
 	finished.connect(Callable(self, "_on_finished"))
-	_play_random()
+	_play_current()
+
+func _shuffle_playlist():
+	playlist = MUSIC_TRACKS.duplicate()
+	playlist.shuffle()
+	current_index = 0
+
+func _play_current():
+	stream = playlist[current_index]
+	play()
 
 func _on_finished():
-	_play_random()
-
-func _play_random():
-	var idx = randi() % MUSIC_TRACKS.size()
-	stream = MUSIC_TRACKS[idx]
-	play()
+	current_index += 1
+	if current_index >= playlist.size():
+		_shuffle_playlist()
+	_play_current()
